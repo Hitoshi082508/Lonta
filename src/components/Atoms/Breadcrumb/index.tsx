316c0b31a6d33eaacfe530fe968a9e004f151bf0
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-15 18:08:43 +0900

    feature: Breadcrumb の機能を実装

--- src/components/Atoms/Breadcrumb/index.tsx
+++ src/components/Atoms/Breadcrumb/index.tsx
@@ -1,5 +1,12 @@
-const Breadcrumb: React.FC = () => {
-  return <></>
+import Link from 'next/link'
+
+type BreadcrumbProps = {
+  title: string
+  link: string
+}
+
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ title, link }) => {
+  return <Link href={link}>{title}</Link>
 }
 
 export default Breadcrumb
