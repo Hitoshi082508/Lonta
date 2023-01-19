af8dd6c4fc1bc5832717f122ca29513b56e5667c
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-20 03:30:19 +0900

    refactor: 最後のパン屑かどうかを判定するフラグを追加

--- src/components/Atoms/Breadcrumb/index.tsx
+++ src/components/Atoms/Breadcrumb/index.tsx
@@ -4,9 +4,10 @@ import { styles } from './styles'
 type BreadcrumbProps = {
   title: string
   link: string
+  isLast: boolean
 }
 
-const Breadcrumb: React.FC<BreadcrumbProps> = ({ title, link }) => {
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ title, link, isLast }) => {
   return (
     <div css={styles.container}>
       <Link href={link} css={styles.link}>
