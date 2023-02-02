3d7c6e847795edbb3d7c83dad4c456cbc6a0ffad
Hitoshi <hitoshi082508@gmail.com>
2023-02-02 21:59:39 +0900

    refactor: mountedが不要になったので削除

--- src/components/Templates/Column/index.tsx
+++ src/components/Templates/Column/index.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { styles } from './styles'
 import ColumnIconPath from '../../../../assets/images/column-icon.png'
 import NextImage from '@/components/Atoms/NextImage'
 import Button from '@/components/Atoms/Button'
 import { Contents } from '@/types/contents'
 import ArticleCard from '@/components/Molecules/ArticleCard'
-import { isMobile } from 'react-device-detect'
 
 type ColumnProps = {
   articles: Contents[]
 }
 
 const Column: React.FC<ColumnProps> = ({ articles }) => {
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => {
-    setMounted(true)
-  })
   return (
     <div css={styles.base}>
       <div css={styles.container}>
@@ -30,10 +25,9 @@ const Column: React.FC<ColumnProps> = ({ articles }) => {
         </div>
         <div css={styles.subTitle}>記事一覧</div>
         <div css={styles.articleContainer}>
-          {mounted &&
-            articles.slice(0, isMobile ? 3 : 6).map((article) => {
-              return <ArticleCard key={article.id} article={article} />
-            })}
+          {articles.slice(0, 6).map((article) => {
+            return <ArticleCard key={article.id} article={article} />
+          })}
         </div>
         <Button text="記事一覧を見る" />
       </div>
