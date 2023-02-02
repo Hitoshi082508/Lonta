28b3fad021ab7ff9e726c979dcee6cb729bf5abf
Hitoshi <hitoshi082508@gmail.com>
2023-02-02 21:54:53 +0900

    refactor: mountedが必要無くなったので削除

--- src/components/Molecules/ArticleCards/index.tsx
+++ src/components/Molecules/ArticleCards/index.tsx
@@ -1,5 +1,5 @@
 import { Contents } from '@/types/contents'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ArticleCard from '@/components/Molecules/ArticleCard'
 import { styles } from './styles'
 
@@ -9,16 +9,11 @@ type ArticleCardsProps = {
 }
 
 const ArticleCards: React.FC<ArticleCardsProps> = ({ articles, length }) => {
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => {
-    setMounted(true)
-  })
   return (
     <div css={styles.container}>
-      {mounted &&
-        articles.slice(0, length).map((article) => {
-          return <ArticleCard key={article.id} article={article} />
-        })}
+      {articles.slice(0, length).map((article) => {
+        return <ArticleCard key={article.id} article={article} />
+      })}
     </div>
   )
 }
