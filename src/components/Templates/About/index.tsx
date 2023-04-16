9a36415dfad64bedd4f6e9534081cde4d75c19c8
MandeNaren <y2pgftpe@gmail.com>
2023-04-16 17:29:01 +0900

    AboutPCデザイン修正版

--- src/components/Templates/About/index.tsx
+++ src/components/Templates/About/index.tsx
@@ -6,23 +6,19 @@ import AboutIconPath from '../../../../assets/images/about-icon.png'
 const About: React.FC = () => {
   return (
     <div css={styles.base}>
-      <div css={styles.container}>
-        <div css={styles.textContainer}>
+      <div css={styles.titleContainer}>
+        <div css={styles.title}>
           <NextImage
             src={AboutIconPath}
             width={19}
             height={28}
             alt="Aboutのアイコン"
           />
-          <h2 css={styles.title}>About</h2>
-          <p css={styles.subTitle}>Lontaとは</p>
-          <div css={styles.description}>
-            大学生の皆さん、長期・有給インターンシップにチャレンジしてみませんか？
-            <br />
-            Lontaは、長期インターンを実際に経験した学生が作った、長期インターンやキャリア構築に関する情報提供を行うメディアです。詳しい職種説明や厳選したインターンの紹介、ここでしか見ることができないコラムなど、長期インターンへの挑戦を充実の機能でサポートします！
-          </div>
-          <button css={styles.button}>Lontaについて詳しく知る</button>
+          <h2 css={styles.titleText}>About</h2>
         </div>
+        <p css={styles.subTitle}>Lontaとは</p>
+      </div>
+      <div css={styles.container}>
         <div>
           <p css={styles.imageTitle}>長期インターンに関するお悩みは</p>
           <p css={styles.imageTitleBottom}>Lontaで解決！</p>
@@ -32,6 +28,21 @@ const About: React.FC = () => {
             height={241}
             alt="おすすめする人々"
           />
+        </div>
+        <div css={styles.textContainer}>
+          <div css={styles.description}>
+            <p>
+              大学生の皆さん、長期・有給インターンシップにチャレンジしてみませんか？
+            </p>
+            <p>
+              Lontaは、長期インターンを実際に経験した学生が作った、長期インターンやキャリア構築に関する情報提供を行うメディアです。
+            </p>
+            <br />
+            <p>
+              詳しい職種説明や厳選したインターンの紹介、ここでしか見ることができないコラムなど、長期インターンへの挑戦を充実の機能でサポートします！
+            </p>
+          </div>
+          <button css={styles.button}>Lontaについて詳しく知る</button>
         </div>
       </div>
     </div>
--- src/components/Templates/About/styles.tsx
+++ src/components/Templates/About/styles.tsx
@@ -4,22 +4,38 @@ import { spacing } from '@/styles/spacing'
 import { sp } from '@/styles/breakpoint'
 
 export const styles = makeStyles({
-  base: (theme) => css`
+  base: () => css`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-    height: 500px;
-    background-color: ${theme.color.lightMain};
+    height: 700px;
   `,
+  titleContainer: () =>
+    css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 30px;
+    `,
+  title: () =>
+    css`
+      display: flex;
+      gap: 10px;
+      align-items: center;
+    `,
+
   container: () =>
     css`
       display: flex;
+      align-items: center;
       gap: ${spacing * 8}px;
       width: 1120px;
       height: 350px;
+      margin-top: 20px;
     `,
   textContainer: () => css``,
-  title: (theme) => css`
+  titleText: (theme) => css`
+    margin-bottom: 8px;
     font-size: ${theme.typography.heading.size.m};
     font-weight: ${theme.typography.weight.bold};
   `,
@@ -29,7 +45,7 @@ export const styles = makeStyles({
     margin-bottom: ${spacing * 10}px;
   `,
   description: (theme) => css`
-    font-size: ${theme.typography.text.size.s};
+    font-size: ${theme.typography.text.size.m};
     color: ${theme.color.black};
     margin-bottom: ${spacing * 10}px;
   `,
